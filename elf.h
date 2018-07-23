833c3d1e8c8e6a48e1eeac97cbdb1732ad24df49
Gregory Norton <gregory.norton@me.com>
2018-07-22 22:24:03 -0400

    Added initial section header.

--- elf.h
+++ elf.h
@@ -14,23 +14,29 @@ extern char *arch;
     } \
 }
 
+typedef uint32_t Elf32_Addr;
+typedef uint32_t Elf32_Off;
+typedef uint16_t Elf32_Half;
+typedef uint32_t Elf32_Word;
+typedef int32_t Elf32_Sword;
+
 #define EI_NIDENT 16
 
 typedef struct {
-    uint8_t e_ident[EI_NIDENT];
-    uint16_t e_type;
-    uint16_t e_machine;
-    uint32_t e_version;
-    uint32_t e_entry;
-    uint32_t e_phoff;
-    uint32_t e_shoff;
-    uint32_t e_flags;
-    uint16_t e_ehsize;
-    uint16_t e_phentsize;
-    uint16_t e_phnum;
-    uint16_t e_shentsize;
-    uint16_t e_shnum;
-    uint16_t e_shstrndx;
+    unsigned char   e_ident[EI_NIDENT];
+    Elf32_Half      e_type;
+    Elf32_Half      e_machine;
+    Elf32_Word      e_version;
+    Elf32_Addr      e_entry;
+    Elf32_Off       e_phoff;
+    Elf32_Off       e_shoff;
+    Elf32_Word      e_flags;
+    Elf32_Half      e_ehsize;
+    Elf32_Half      e_phentsize;
+    Elf32_Half      e_phnum;
+    Elf32_Half      e_shentsize;
+    Elf32_Half      e_shnum;
+    Elf32_Half      e_shstrndx;
 } ELF32_Ehdr;
 
 /* e_ident indeces */
@@ -87,10 +93,23 @@ typedef struct {
 #define ET_LOPROC           0xFF00
 #define ET_HIPROC           0xFFFF
 
+#include "elf_machines.h"
+
 ELF32_Ehdr *new_ELF32_Ehdr(uint16_t e_machine);
 void write_ELF32_Ehdr(ELF32_Ehdr *hdr, FILE *out_file);
 
-#include "elf_machines.h"
+typedef struct {
+    Elf32_Word  sh_name;
+    Elf32_Word  sh_type;
+    Elf32_Word  sh_flags;
+    Elf32_Addr  sh_addr;
+    Elf32_Off   sh_offset;
+    Elf32_Word  sh_size;
+    Elf32_Word  sh_link;
+    Elf32_Word  sh_info;
+    Elf32_Word  sh_addralign;
+    Elf32_Word  sh_entsize;
+} ELF32_Shdr;
 
 typedef struct {
     uint32_t p_type;
